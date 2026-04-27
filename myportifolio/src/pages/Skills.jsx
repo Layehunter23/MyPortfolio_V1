e8b3b39a68e67aa104a742014c3ae8b4b0eb4a6c
Abdulahi-23 <papa.a.barry@gmail.com>
2026-04-27 01:35:27 +0000

    Making It Responsive 3/3

--- myportifolio/src/pages/Skills.jsx
+++ myportifolio/src/pages/Skills.jsx
@@ -4,18 +4,18 @@ const Skills = () => {
         <div id="skills"className="flex flex-col justify-center items-center  mt-10 mb-20">
         <h1 className="text-3xl text-blue-100 text-center">Skills</h1><br></br>
         <h3 className="text-2xl text-blue-100">- Languages</h3><br></br>
-        <div className="grid grid-cols-6 gap-10 sm:justify-items-center sm:mx-10 max-sm:mx-10">
-        <img className ="h-35 rounded-3xl max-sm: " src="/javascript.svg" alt="JavaScript" />
+        <div className="grid grid-cols-6 gap-2 sm:justify-items-center sm:mx-10 max-sm:mx-10">
+        <img className ="h-35 rounded-3xl max-sm:rounded-3xl" src="/javascript.svg" alt="JavaScript" />
         <img className ="h-35" src="/python.svg" alt="Python" />
         <img className="h-35 rounded-3xl" src="/java.svg" alt="Java" />
         <img className="h-35" src="php.svg" alt="PHP" />
         <img className="h-35" src="C.svg" alt="C" />
         <img className="h-35" src="html.svg" alt="HTML" />
-        <img className="h-35" src="css.svg" alt="CSS" />
+        <img className="h-35 max-sm:h-15" src="css.svg" alt="CSS" />
         </div><br></br>
         <h3 className="text-2xl text-blue-100">- Frameworks</h3><br></br>
-        <div className="grid grid-cols-5 gap-10 sm:mx-10 max-sm:mx-10 max-sm:object-contain">
-            <img className="block h-35" src="/django.svg" alt="Django" />
+        <div className="grid grid-cols-5 gap-2 sm:mx-10 max-sm:mx-10 max-sm:object-contain">
+            <img className="h-35 " src="/django.svg" alt="Django" />
             <img className="h-35" src="/react.svg" alt="ReactJS"/> 
             <img className="h-35" src="nodejs.svg" alt="NodeJS" />
             <img className="h-35" src="/tailwindcss.svg" alt="TailwindCSS" />
@@ -23,11 +23,11 @@ const Skills = () => {
          </div>
 
          <h3 className="text-2xl text-blue-100 mt-5">- Other Tools</h3><br></br>
-         <div className="grid grid-cols-5 gap-10 md:mx-10">
-            <img className="block h-35 max-sm:object-contain" src="git.svg" alt="Git" />
-            <img className="h-35 bg-white rounded-2xl" src="/github.svg" alt="Github"/> 
-            <img className="h-35 bg-white rounded-2xl" src="/mysql.svg" alt="MySQL"/> 
-            <img className="h-35" src="/mongodb.svg" alt="MongoDB"/> 
+         <div className="grid grid-cols-5 gap-2 md:mx-10 max-sm:ml-10 max-sm:mb-0.2">
+            <img className="block h-35" src="git.svg" alt="Git" />
+            <img className="h-35 bg-white rounded-2xl  max-sm:object-contain max-sm:h-20" src="/github.svg" alt="Github"/> 
+            <img className="h-35 bg-white rounded-2xl max-sm:object-contain max-sm:h-20" src="/mysql.svg" alt="MySQL"/> 
+            <img className="h-35 max-sm:h-20" src="/mongodb.svg" alt="MongoDB"/> 
         </div>
         </div>
         </>
